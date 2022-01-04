e461669b71d7fd180dcfd81be3348ba3309b4eb6
HenriqueHartmann <alemaohartmann@gmail.com>
2022-01-03 23:57:28 -0300

    add assets, background image and contacts container

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,23 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: const Text('Dashboard'),),
+          appBar: AppBar(title: const Text('Dashboard')),
+          body: Column(
+            children: <Widget>[
+              Image.asset('images/bytebank_logo.png'),
+              Container(
+                width: 120,
+                height: 100,
+                color: Colors.green,
+                child: Column(
+                  children: const <Widget>[
+                    Icon(Icons.people),
+                    Text('Contacts')
+                  ],
+                )
+              ),
+            ],
+          ),
         )
     );
   }
