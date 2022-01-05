3b32f08c512c5161a608cb35007af15f7866639f
Henrique Luiz Hartmann <hhartmann@isolutions.com.br>
2022-01-05 15:57:14 -0300

    config ListView builder

--- lib/screens/contatcts_list.dart
+++ lib/screens/contatcts_list.dart
@@ -1,23 +1,28 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-  const ContactsList({Key? key}) : super(key: key);
+  ContactsList({Key? key}) : super(key: key);
+
+  final List<Contact> contacts = [];
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    contacts.add(Contact(0, 'Alex', 1000));
+    contacts.add(Contact(0, 'Julio', 2000));
+    contacts.add(Contact(0, 'Amanda', 3000));
+
     return Scaffold(
       appBar: AppBar(title: const Text('Contacts')),
-      body: ListView(
-        children: const <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('Alex', style: TextStyle(fontSize: 24.0),),
-              subtitle: Text('1000', style: TextStyle(fontSize: 16.0)),
-            )
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+
+          return _ContactItem(contact: contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -34,4 +39,25 @@ class ContactsList extends StatelessWidget {
       ),
     );
   }
+}
+
+class _ContactItem extends StatelessWidget {
+  const _ContactItem({Key? key, required this.contact}) : super(key: key);
+
+  final Contact contact;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+        child: ListTile(
+          title: Text(contact.name, style: const TextStyle(fontSize: 24.0),),
+          subtitle:
+          Text(contact.accountNumber.toString(),
+               style: const TextStyle(fontSize: 16.0)
+          ),
+        )
+    );
+  }
+  
 }
